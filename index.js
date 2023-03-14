1fb48b9d06e600560031fa2929f28e3cff113b17
joaovicttor_c <109038802+joaovicttor-c@users.noreply.github.com>
2023-03-14 11:23:33 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -21,3 +21,41 @@ function openMenu() {
     body.classList.add("menu-expanded");
    };
   }
+
+
+
+		var i = 0;
+		var tag = document.querySelector("h1");
+		var html = document.querySelector("h1").innerHTML;
+		var attr = tag.setAttribute("data", html);
+    console.log(attr);
+		var txt = tag.getAttribute("data");
+		var speed = 170;
+
+		function typeWriter() {
+		  if (i <= txt.length) {
+		    document.querySelector("h1").innerHTML = txt.slice(0 , i + 1);
+		    i++;
+		    setTimeout(typeWriter, speed);
+		  }
+		}
+
+typeWriter();
+
+ScrollReveal({
+		origin: 'top',
+    distance: '30px',
+    duration: 1000,
+	}).reveal(`
+		.mysite-Navigation, 
+		.mysite-Home, 
+		.mysite-About, 
+		.mysite-About-container > h2, 
+		.mysite-About-container > h3, 
+		.mysite-About-container > p, 
+		.mysite-Works,
+		.mysite-Works-links
+`);
+
+ScrollReveal().reveal('.mysite-Home-container > p', {delay: 500})
+ScrollReveal().reveal('.mysite-Home-container-buttons > li', {delay: 800})
